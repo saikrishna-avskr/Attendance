8f1a55c1bc82c6a5c4620cb6d18d342f098d9559
A V <147173461+saikrishna-avskr@users.noreply.github.com>
2024-05-13 19:34:13 +0530

    Update professors.js

--- frontend/src/components/professors.js
+++ frontend/src/components/professors.js
@@ -17,6 +17,33 @@ function Professors() {
     return (
         <div>
             <h1>{prof[0].hod}</h1>
+          <h3>Professor detils:</h3>
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered">
+            <tr>
+              <th scope="col" >Prof code</th>
+              <th scope="col" >Name</th>
+              <th scope="col" >Class Teacher</th>
+              <th scope="col" >Subjects</th>
+              <th scope="col" >Attendence</th>
+            </tr>
+            { for x in prof }
+            <tr>
+              <th scope="row">{{ x.prof_code }}</th>
+              <td>{{ x.prof_name }}</td>
+              <td>{{ x.generic_name }}</td>
+              <td>{{x.isClass_teacher}}</td>
+              <td>{}</td>
+              { if x.isHOD }
+              <td><a href="#" class="btn btn-warning ">Remove Profile</a></td>
+              {else}
+              <td><a href="#" class="btn btn-warning disabled">Remove Profile</a></td>
+              {endif}
+            </tr>
+            { endfor }
+          </table>
+        </div>
+      
         </div>
     )
 }
